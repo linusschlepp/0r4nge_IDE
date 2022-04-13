bed7c8813eaf9e5a68eae1a535ce082069787438
linus <linusmo.94@googlemail.com>
2022-04-13 15:28:46 +0200

    files get now updated automatically

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-import json
 import os
 import subprocess
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter.ttk import *
+from popup import PopUp
 from pathlib import Path
-
-
-
-# def select_file():
-#     file = open('currentFile.txt', 'w')
-#     file_name = askopenfilename()
-#     try:
-#         file.write(str(Path(file_name.name)))
-#     except AttributeError:
-#         pass
-#     file.close()
-#     my_win.t3.insert(1.0, "")
 
 
 class Window:
@@ -24,30 +12,12 @@ class Window:
         self.win = win
         self.tree_view = Treeview(win)
         self.tree_view['columns'] = ("Name")
-      #  self.tree_view.column("#0", width=120, minwidth=25, stretch=NO)
-       #  self.tree_view.heading("#0", text="Name", anchor=W)
-       # self.tree_view.column("Name", width=150, minwidth=150, stretch=NO)
-        # self.tree_view.column("Name", anchor=W, width=170)
         self.t3 = Text(win, height=50, width=130)
         self.t3.place(x=500, y=5)
 
-        #  self.menu_bar = Menubutton(window, image=PhotoImage(file='addIcon.png'))
-        # self.menu_bar = Menu(window)
-        #  self.menu_bar.grid(row=0, column=0)
-
-        # self.file_menu = Menu(self.menu_bar)
-        # self.menu_bar.config(menu=self.file_menu)
-
-        # self.file_menu.add_command(label="Execute", command=lambda: self.create_file(self.t3.get("1.0", END)))
-        # self.file_menu.add_command(label="Select", command=lambda: self.select_file())
-        # self.file_menu.add_command(label="Add", command=lambda: new_file())
-
-        # self.menu_bar.add_cascade(label="File", menu=self.file_menu)
-        #  window.config(menu=self.menu_bar)
-
         if open('currentFile.txt').read() is not None:
             try:
-                # self.t3.insert(1.0, open(open('currentFile.txt').read()).read())
+
                 self.text = StringVar()
                 dir = str(open('currentFile.txt').read())
                 self.text.set(dir)
@@ -75,14 +45,11 @@ class Window:
         self.btn_select = Button(win, text='Select',
                                  command=lambda: self.select_file())
         self.btn_new = Button(win, text='Add',
-                              command=lambda: self.new_file())
-
-        # self.tree_view.heading("#0", text="Label", anchor=W)
-        # self.tree_view.heading("Name", text="Name", anchor=W)
-        # self.tree_view.insert(parent='', index='end', iid=0, text="Parent", values=("Test"))
+                              command=lambda: self.add_file())
 
         self.tree_view.pack(side=TOP, fill=X)
-        self.tree_view.bind("<Double-1>", self.OnDoubleClick)
+        self.t3.bind("<Key>", lambda event: self.change_file())
+        self.tree_view.bind("<Double-1>", self.on_double_click)
         self.tree_view.place(x=5, y=100)
         self.btn_execute.place(x=5, y=5)
         self.btn_select.place(x=90, y=5)
@@ -91,7 +58,12 @@ class Window:
         window.geometry('1700x800+10+10')
         window.mainloop()
 
-    def OnDoubleClick(self, event):
+    def change_file(self):
+        file = open(self.current_file, 'w')
+        file.write(self.t3.get("1.0", END))
+        file.close()
+
+    def on_double_click(self, event):
         current_item = self.tree_view.item(self.tree_view.focus())
         self.current_file = self.text.get() + "\\" + str(current_item["text"]).strip("['").strip("']'")
         self.lbl_file.destroy()
@@ -125,39 +97,14 @@ class Window:
 
         self.t3.delete(1.0, END)
         self.file.close()
-        #  self.lbl_path.destroy()
-        # self.lbl_path = Label(self.win, text=str(Path(self.file_name)),
-        # font=('Helvetica', 10, 'bold italic'))
-        # self.btn_select.place(x=90, y=240)
         self.text.set(str(Path(self.file_name)))
         self.t3.insert(1.0, open(str(Path(self.file_name))).read())
 
-    def popupwin(self):
-        # Create a Toplevel window
-        self.top = Toplevel(self.win)
-        self.top.geometry("750x250")
-        self.top.title("Enter name")
-
-        # Create an Entry Widget in the Toplevel window
-        self.entry = Entry(self.top, width=25)
-        self.entry.pack()
-
-        # Create a Button to print something in the Entry widget
-        #  Button(top, text="Ok", command=lambda: insert_val(entry)).pack(pady=5, side=TOP)
-        # Create a Button Widget in the Toplevel Window
-        self.button = Button(self.top, text="Ok", command=lambda: self.top.destroy())
-        self.button.pack(pady=5, side=TOP)
-
-        # Create a Label
-    #    self.label = Label(self.win, text="Click the Button to Open the Popup Dialogue", font=('Helvetica 15 bold'))
-    #    self.label.pack(pady=20)
-
-        self.new_file = self.entry.get()
-
-    def new_file(self):
-        file_name = input("enter file name: ")
-        self.tree_view.insert('', END, values=file_name+".py")
-        file_name = self.text.get() + "\\" + file_name+".py"
+    def add_file(self):
+        file_name = PopUp(self.win)
+        self.win.wait_window(file_name.top)
+        self.tree_view.insert(parent='0', index='end', text=str(file_name.value) + ".py")
+        file_name = self.text.get() + "\\" + str(file_name.value) + ".py"
         try:
             open(file_name, 'w')
         except AttributeError:
---
+++ popup.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+
+class PopUp(Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+
+        self.top = Toplevel(master)
+        self.top.geometry("750x250")
+        self.top.title("Enter name")
+
+        self.label = Label(self.top, text="Enter name of new file")
+        self.label.pack()
+        self.entry = Entry(self.top)
+        self.entry.pack()
+        self.value = self.entry.get()
+        self.button = Button(self.top, text='Ok', command=self.set_close)
+        self.button.pack()
+
+    def set_close(self):
+        self.value = self.entry.get()
+        self.top.destroy()
+
+
+
+
